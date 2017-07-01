3e5fd1c7b62f9d18486bfe9ccdc60eed0ae24ed6
Jictyvoo <jictyvoo.ecomp@gmail.com>
2017-06-30 23:52:25 -0300

    Atualizado o Index
    Inseridas fotos, e informações no index.php.

--- view/index.php
+++ view/index.php
@@ -28,34 +28,33 @@
 		</ol>
 		<div class="carousel-inner" role="listbox">
 			<div class="item active">
-				<img class="first-slide" src="imagens/carousel_1.jpg" alt="First slide">
+				<img class="first-slide" src="imagens/carousel/carousel_1.jpg" alt="First slide">
 				<div class="container">
 					<div class="carousel-caption">
-						<h1>Example headline.</h1>
-						<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-						<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
+						<h1>Cálculo de pontos de iluminação e tomadas de residências.</h1>
+						<p>Calcule quantidades de tomadas necessárias em uma residência, e potência de iluminação através da área e perímetro.</p>
+						<p><a class="btn btn-lg btn-primary" href="gerenciadorView.php" role="button">Abrir Sistema agora</a>
 						</p>
 					</div>
 				</div>
 			</div>
 			<div class="item">
-				<img class="second-slide" src="imagens/carousel_2.jpg" alt="Second slide">
+				<img class="second-slide" src="imagens/carousel/carousel_2.jpg" alt="Second slide">
 				<div class="container">
 					<div class="carousel-caption">
-						<h1>Another example headline.</h1>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-						<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a>
-						</p>
+						<h1>Sobre o sistema</h1>
+						<p>Nosso sistema pode ser usado por indústrias comércios e residências ele consegue suprir a necessidade de todos.</p>
+						<!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>-->
 					</div>
 				</div>
 			</div>
 			<div class="item">
-				<img class="third-slide" src="imagens/carousel_3.jpg" alt="Third slide">
+				<img class="third-slide" src="imagens/carousel/carousel_3.jpg" alt="Third slide">
 				<div class="container">
 					<div class="carousel-caption">
-						<h1>One more for good measure.</h1>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-						<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>
+						<h1>Uso Responsavel.</h1>
+						<p>Com ajuda de nosso sistema será usado apenas o necessário evitando o desperdício de material e energia elétrica.</p>
+						<!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>-->
 						</p>
 					</div>
 				</div>
@@ -63,11 +62,11 @@
 		</div>
 		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
+			<span class="sr-only">Anterior</span>
 		</a>
 		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
+			<span class="sr-only">Próximo</span>
 		</a>
 	</div>
 	<!-- /.carousel -->
@@ -89,35 +88,47 @@
 				<p class="lead">Somos uma pequena equipe que trabalha no desenvolvimento de aplicações em web para auxiliar no trabalho de pessoas em certos momentos.</p>
 			</div>
 			<div class="col-md-5">
-				<img class="featurette-image img-responsive center-block" data-src="imagens/carousel_1.jpg" alt="Generic placeholder image">
+				<img class="featurette-image img-responsive center-block" src="imagens/tutorial/grupo.jpg" alt="Generic placeholder image">
 			</div>
 		</div>
 
 		<hr class="featurette-divider">
 
 		<div class="row featurette">
-			<div class="col-md-7 col-md-push-5">
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive center-block" src="imagens/tutorial/inicio.jpg" alt="Generic placeholder image">
+			</div><br/>
+			<div class="col-md-7">
 				<h2 class="featurette-heading">Como usar a aplicação. <span class="text-muted">Guia Passo a Passo.</span></h2>
 				<p class="lead">Inicialmente, ao abrir o sistema, você estará em uma tela de inserção de comodos, podendo mudar de abas livremente enquanto utiliza o mesmo sistema.</p>
+			</div>
+		</div>
+
+		<div class="row featurette">
+			<div class="col-md-12">
+				<img class="featurette-image img-responsive center-block" src="imagens/tutorial/Inserir Comodo-Tutorial.png" alt="Generic placeholder image">
+			</div>
+			<div class="col-md-12">
 				<p class="lead">Na aba de Inserção de Comodos, você irá inserir os dados correspondentes aos comodos que deseja inserir, como seu tipo, tamanho, e tomadas especificas.</p>
 			</div>
-			<div class="col-md-5 col-md-pull-7">
-				<img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			<div class="col-md-12">
+				<img class="featurette-image img-responsive center-block" src="imagens/tutorial/comodos inseridos-Tutorial.png" alt="Generic placeholder image">
+			</div>
+				<div class="col-md-12">
+				<p class="lead">Na aba de Comodos inseridos, você terá acesso a todos os comodos já inseridos, podendo removê-los, ou editá-los, como bem entender.</p>
 			</div>
 		</div>
 
 		<hr class="featurette-divider">
 
 		<div class="row featurette">
-			<div class="col-md-7">
-				<h2 class="featurette-heading">Abrir Sistema. <span class="text-muted">Acessando o sistema.</span></h2>
-				<li>
-					<a href = "gerenciadorView.php">Usar Sistema</a>
-				</li>
-				<p class="lead">Clique agora e utilize nosso sistema</p>
-			</div>
 			<div class="col-md-5">
-				<img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+				<h2 class="featurette-heading">Abrir Sistema. <span class="text-muted">Acessando o sistema.</span></h2>
+				<a href = "gerenciadorView.php"><button type="button" class="btn btn-secondary btn-lg btn-block">Usar Sistema</button></a>
+				<p class="lead">Clique agora, utilize nosso sistema, e tenha seu resultado de forma imediata</p>
+			</div>
+			<div class="col-md-7">
+				<img class="featurette-image img-responsive center-block" src="imagens/tutorial/Resultados - Tutorial.png" alt="Generic placeholder image">
 			</div>
 		</div>
 
